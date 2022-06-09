96ab299be599cb40ce0a06f10187ce291336adf6
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-09 11:27:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+   return ['Antonia','Nuru']
+}
+function returnLastTwoDrivers(){
+    return ['Amari', 'Mo']
+}
+
